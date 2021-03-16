ad1e6cfacce489b1e23f4aad3fbc22be9e89181f
FYSwallow <1945472730@qq.com>
2021-03-16 22:42:25 +0800

    添加范例

---
+++ web/child/create-ui/README.md
@@ -0,0 +1,5 @@
+# 代码规范
+
+```
+npm i -d eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin
+```
---
+++ web/child/create-ui/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div className="App">
+            <div>
+                <span>{true ? <div>123</div> : 456}</span>
+            </div>
+        </div>
+    )
+}
+
+export default App
