fe7dcf9296eac2c3a6660e88d94f57f5ba8b4b4e
fuyue <1945472730@qq.com>
2021-04-17 18:07:04 +0800

    添加拓拽示例

---
+++ web/parent/src/views/drag3/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from './Container'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
+function Drag() {
+    return (
+        <div className="App">
+            <DndProvider backend={HTML5Backend}>
+                <Container />
+            </DndProvider>
+        </div>
+    )
+}
+export default Drag
